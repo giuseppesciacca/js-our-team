d29af1e8c4427fca3e7ad9f044eb593c1ef8ac90
giuseppesciacca <sciaccagi@gmail.com>
2023-03-22 15:34:32 +0100

    [add] array ourTeam

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,37 @@ MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di n
 MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1: Trasformare la stringa foto in una immagine effettiva
 BONUS 2: Organizzare i singoli membri in card/schede */
+
+//array di oggetti per rappresentare i membri del team.
+const ourTeam = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
